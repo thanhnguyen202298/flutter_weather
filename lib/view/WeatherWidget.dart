90dd8a3d87485073a169a839c7ea316a10025b96
nguyen trung thanh <thanh@fuvi.vn>
2021-02-22 09:57:24 +0700

    add weather widget

--- lib/view/WeatherWidget.dart
+++ lib/view/WeatherWidget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_weather/model/ModelOpenWeather.dart';
 import 'package:flutter_weather/utils/ConstantData.dart';
 import 'package:flutter_weather/view/BlocProvider.dart';
+import 'package:flutter_weather/view/CaptionText.dart';
 import 'package:flutter_weather/view/DecorMyWidget.dart';
 
 /**
@@ -10,8 +12,8 @@ import 'package:flutter_weather/view/DecorMyWidget.dart';
  * victo202298@gmail.com
  */
 class WeatherWidget extends StatelessWidget {
-  final Daily _infoWeather;
-  WeatherWidget(this._infoWeather);
+  final Daily _dateWeather;
+  WeatherWidget(this._dateWeather);
   @override
   Widget build(BuildContext context) {
     final bloc = BlocProvider.of(context);
@@ -20,7 +22,36 @@ class WeatherWidget extends StatelessWidget {
       mainAxisSize: MainAxisSize.max,
       children: [
         Container(
-          decoration: CircleDecor("$IconWeatherLinkPrefix/${_infoWeather.weather[0].icon}@2x.png").build(),
+          decoration: CircleDecor("$IconWeatherLinkPrefix/${_dateWeather.weather[0].icon}@2x.png").build(),
+        ),
+        Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Row(
+              children: [
+                CaptionText("Date: ",bold: FontWeight.bold, fontsize: 14,),
+                CaptionText("${_dateWeather.date}", color: Colors.black54,bold: FontWeight.bold, fontsize: 14,),
+              ],
+            ),
+            Row(
+              children: [
+                CaptionText("Presure: ",bold: FontWeight.bold, fontsize: 14,),
+                CaptionText("${_dateWeather.pressure} at", color: Colors.black54,bold: FontWeight.bold, fontsize: 14,),
+              ],
+            ),
+            Row(
+              children: [
+                CaptionText("Temperature: ",bold: FontWeight.bold, fontsize: 14,),
+                CaptionText("${_dateWeather.temp.day}°C", color: Colors.black54,bold: FontWeight.bold, fontsize: 14,),
+              ],
+            ),
+            Row(
+              children: [
+                CaptionText("Humidity: ",bold: FontWeight.bold, fontsize: 14,),
+                CaptionText("${_dateWeather.humidity}%", color: Colors.black54,bold: FontWeight.bold, fontsize: 14,),
+              ],
+            ),
+          ],
         )
       ],
     );
